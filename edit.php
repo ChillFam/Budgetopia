d85cb25149f91dea893102299f5b7a22da134705
Ankit Verma <72822442+ChillFam@users.noreply.github.com>
2020-11-12 19:54:12 -0800

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,3 +1,14 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,16 +22,17 @@
 <body>
     <nav class="prim-text sec-back">
         <ul>
-            <li><a href="index.html">Budgetopia</a></li>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="savings.html">Savings</a></li>
+            <li><a href="index.php">Budgetopia</a></li>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="savings.php">Savings</a></li>
+            <li><a href="edit.php">Edit</a></li>
             <li><a href="addExpense.html">Add Expense</a></li>
-            <li><a href="edit.html">Edit</a></li>
+            <li><a href="logout.php">Logout</a></li>
         </ul>
     </nav>
     <div>
         <p>
-            Name: *user name*
+             Username: <?php echo htmlspecialchars($_SESSION["username"]); ?>
         </p>
         <p>
             Income: *income*
