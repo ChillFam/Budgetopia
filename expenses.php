161d33e0f1a5813d553d0db9d13839e9958a8620
gmontarano <gmontarano@users.noreply.github.com>
2020-11-23 19:34:54 -0800

    Update expenses.php

--- expenses.php
+++ expenses.php
@@ -48,7 +48,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
     <nav class="prim-text sec-back">
         <ul>
-            <li><a href="index.php">Budgetopia</a></li>
+            <li><a href="home.php">Budgetopia</a></li>
             <li><a href="home.php">Home</a></li>
             <li><a href="savings.php">Savings</a></li>
             <li><a href="edit.php">Edit</a></li>
